3be4e8b0610de28c94f595f83765586a80cbf812
DavidPedraza95 <82422713+DavidPedraza95@users.noreply.github.com>
2021-10-07 21:22:48 -0400

    fresh code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from "./components/wrapper/Wrapper"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      < Wrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+           <h1>Dynamic Employee Directory</h1>
+           <p>Welcome to the directory. Please enter a name below.</p>
+        </div>
+    )
+}
+
+export default Header;
